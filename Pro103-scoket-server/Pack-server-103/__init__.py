5fe0eb1e55c3189b2658650b37eb626166e10a23
htys8899 <495373856@qq.com>
2017-10-17 16:08:58 +0800

    1005--1017

--- Pro103-scoket-server/Pack-server-103/__init__.py
+++ Pro103-scoket-server/Pack-server-103/__init__.py
@@ -5,10 +5,10 @@ host = socket.gethostname()
 port = 12345               
 s8.bind((host, port))      
 s8.listen(5)                 
-print "this is server side1005, waiting for client to connect "
+print "this is server side1017, waiting for client to connect "
 while True:
     c, addr = s8.accept()    
     print  "why next connection the addr will add +1??"
     print 'connection address:', addr
-    c.send('welcome to cainiao')
+    c.send('welcome to cainiao 20171017')
     c.close()   
